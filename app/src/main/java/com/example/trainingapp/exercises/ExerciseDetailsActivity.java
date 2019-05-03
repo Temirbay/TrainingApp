43a40eb378c3e5b26dd3ed3fc3b52f42f4a59d29
Miras <temirbaymiras.ktl@gmail.com>
2019-05-03 18:39:41 +0600

    background added

--- app/src/main/java/com/example/trainingapp/exercises/ExerciseDetailsActivity.java
+++ app/src/main/java/com/example/trainingapp/exercises/ExerciseDetailsActivity.java
@@ -2,11 +2,23 @@ package com.example.trainingapp.exercises;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.trainingapp.R;
 import com.example.trainingapp.model.Exercise;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ExerciseDetailsActivity extends AppCompatActivity {
+
+    @BindView(R.id.gif)
+    ImageView gif;
+
+    @BindView(R.id.body)
+    TextView body;
 
     private Exercise exercise;
 
@@ -14,7 +26,11 @@ public class ExerciseDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_details);
-
-        exercise = getIntent().getParcelableExtra("exercise");
+        ButterKnife.bind(this);
+        exercise = (Exercise)getIntent().getExtras().get("exercise");
+        if (exercise != null) {
+            Glide.with(this).asGif().load(exercise.gif).into(gif);
+            body.setText(exercise.body);
+        }
     }
 }
